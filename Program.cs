7c4beee5ee688fbefe2e564d4889070b354ee913
Alex-dev1356 <asoliven26@yahoo.com>
2026-05-09 15:30:16 +0800

    Adding Services from Identity Core that we needed

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 
+using AuthECAPI.Models;
+using Microsoft.AspNetCore.Identity;
+
 namespace AuthECAPI
 {
     public class Program
@@ -13,6 +16,19 @@ namespace AuthECAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            //Adding Services from Identity Core
+            //For Identity Manager
+            builder.Services
+                .AddIdentityApiEndpoints<IdentityUser>()//This is responsible for adding the endpoints for Identity API Core
+                                                        //The IdentityUser is the default user class provided by ASP.NET Core Identity.
+                                                        //It includes properties like UserName, Email, PasswordHash, etc.
+                                                        //You can also create a custom user class that inherits from IdentityUser
+                                                        //if you need to add additional properties specific to your application.
+            .AddEntityFrameworkStores<ApplicationDbContext>(); //This is responsible for adding the stores for Identity API Core
+                                                                //The ApplicationDbContext is the database context that will be used to store the identity data.
+                                                                //You need to create this class and configure it to use your database provider (e.g., SQL Server, SQLite, etc.)
+                                                                //It should inherit from IdentityDbContext<IdentityUser> or a custom user class if you created one.
 
             var app = builder.Build();
 
