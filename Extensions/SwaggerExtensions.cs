fe99336bb21f120b7d9b21126a17ad49e1098d90
Alex-dev1356 <asoliven26@yahoo.com>
2026-07-08 15:32:10 +0800

    Configuring the Swagger API Explorer for passing the JSON Web Token directly on the browser once the Swagger UI is up once you run the AuthECAPI

--- Extensions/SwaggerExtensions.cs
+++ Extensions/SwaggerExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.OpenApi.Models;
 
 namespace AuthECAPI.Extensions
 {
@@ -7,7 +8,33 @@ namespace AuthECAPI.Extensions
         public static IServiceCollection AddSwaggerExplorer(this IServiceCollection services)
         {
             services.AddEndpointsApiExplorer()
-                     .AddSwaggerGen();
+                     .AddSwaggerGen(options =>
+                     {
+                        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                        {
+                            In = ParameterLocation.Header,
+                            Description = "Fill in the JWT token",
+                            Name = "Authorization",
+                            Type = SecuritySchemeType.Http,
+                            Scheme = "Bearer",
+                            BearerFormat = "JWT"
+                        });
+
+                        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                        {
+                            {
+                                new OpenApiSecurityScheme
+                                {
+                                    Reference = new OpenApiReference
+                                    {
+                                        Type = ReferenceType.SecurityScheme,
+                                        Id = "Bearer"
+                                    }
+                                },
+                                new List<string>()
+                            }
+                        });
+                    });
             return services;
         }
 
